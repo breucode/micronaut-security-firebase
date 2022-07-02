e6dba0362b08b6cf5c39517bb2ee8a7bc709014b
breucode <pbreuer@breuco.de>
2022-07-02 17:52:25 +0200

    Bump micronaut from 3.4.3 to 3.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 java { toolchain.languageVersion.set(JavaLanguageVersion.of(8)) }
 
-micronaut { version("3.4.3") }
+micronaut { version("3.5.2") }
 
 val pomDesc = "Validation of Firebase tokens in Micronaut Security"
 val artifactName = "micronaut-security-firebase"
